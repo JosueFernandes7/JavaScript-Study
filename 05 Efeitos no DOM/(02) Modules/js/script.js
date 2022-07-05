a334df0fc7197334858029434933d48de8317394
Josue Fernandes <josue7mf@hotmail.com>
2022-07-05 15:37:11 -0300

    modules 2

--- 05 Efeitos no DOM/(02) Modules/js/script.js
+++ 05 Efeitos no DOM/(02) Modules/js/script.js
@@ -1,9 +1,13 @@
 
-import initScrollSuave from './modules/scroll-suave.js'
+import initScrollSuave from './modules/scroll-suave.js';
 import initAnimatelScroll from './modules/scroll.animacao.js';
 import initFaqAccordion from './modules/accordion.js'
-import initTabNav from './modules/tabnav.js'
+import initTabNav from './modules/tabnav.js';
+
+import * as teste from './modules/teste.js'
 initScrollSuave();
 initAnimatelScroll();
 initFaqAccordion();
 initTabNav();
+console.log(teste.senha,teste.senha1);
+console.log(teste);
