0e24d437dfd8c01f88517be43cbb71d00202cbe1
Josue Fernandes <josue7mf@hotmail.com>
2022-07-09 18:13:50 -0300

    outsideclick

--- 05 Efeitos no DOM/(04) Dropdown Menu/js/modules/outsideclick.js
+++ 05 Efeitos no DOM/(04) Dropdown Menu/js/modules/outsideclick.js
@@ -1,24 +1,4 @@
-export default function initDropDownMenu() {
-  console.log('teste')
-}
-
-const dropDownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropDownMenus.forEach(menu => {
-  ['click','touchstart'].forEach(event => {
-    menu.addEventListener(event,showMenu);
-  })
-})
-
-function showMenu(e) {
-  e.preventDefault();
-  this.classList.add('ativo')
-  outSideClick(this,['click','touchstart'],() => {
-    this.classList.remove('ativo')
-  });
-}
-
-function outSideClick(element,eventos,callback) {
+export default function outSideClick(element,eventos,callback) {
   const outside = 'data-outside';
   
   const html = document.documentElement;
