b3eb3f3b492f239b71da035d875fded418fb67a2
Josue Fernandes <josue7mf@hotmail.com>
2022-07-09 21:55:57 -0300

    setTimeout and interval

--- 05 Efeitos no DOM/(04) Dropdown Menu/js/modules/dropdown-menu.js
+++ 05 Efeitos no DOM/(04) Dropdown Menu/js/modules/dropdown-menu.js
@@ -1,3 +1,4 @@
+import outSideClick from "./outsideclick.js";
 export default function initDropDownMenu() {
   console.log('teste')
 }
@@ -18,26 +19,3 @@ function showMenu(e) {
   });
 }
 
-function outSideClick(element,eventos,callback) {
-  const outside = 'data-outside';
-  
-  const html = document.documentElement;
-  if(!element.hasAttribute(outside)) {
-    eventos.forEach(userEvent => {
-      html.addEventListener(userEvent,handleOutSideClick);
-    })
-    element.setAttribute(outside,'');
-    
-  }
-  
-  function handleOutSideClick(event) {
-    if(!element.contains(event.target)) { // verifica se o click nao foi no element, se nao foi, executa a função de remover class e vai fecha-lo
-      element.removeAttribute(outside)
-      eventos.forEach(userEvent => {
-        html.removeEventListener(userEvent,handleOutSideClick);
-      })
-      callback();
-    }
-    
-  }
-}
